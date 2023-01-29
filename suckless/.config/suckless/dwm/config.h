860eda70219811e40df773c6609db932c7622b9e
adrianbalbs <adrianbalbs@hotmail.com>
2023-01-29 15:58:16 +1100

    Changed some settings

--- suckless/.config/suckless/dwm/config.h
+++ suckless/.config/suckless/dwm/config.h
@@ -72,12 +72,16 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
 static const char *browsercmd[] = { "firefox", NULL};
+static const char *htopcmd[] = { "alacritty", "-e", "htop", NULL };
+static const char *lfcmd[] = { "alacritty", "-e", "lf", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lfcmd } },
+	{ MODKEY|ShiftMask,             XK_h,      spawn,          {.v = htopcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
