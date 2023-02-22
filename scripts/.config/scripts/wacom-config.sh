7d6d9391ccb3c7a26c6728d7f530ccecef88a7b4
adrianbalbs <adrianbalbs@hotmail.com>
2023-02-23 09:33:35 +1100

    fixed wacom scripts

--- scripts/.config/scripts/wacom-config.sh
+++ scripts/.config/scripts/wacom-config.sh
@@ -9,7 +9,7 @@ done
 
 list=$(xsetwacom list devices)
 pad=$(echo "${list}" | awk '/pad/{print $7}')
-stylus=$(echo "${list}" | xsetwacom list devices | awk '/stylus/{print $7}')
+stylus=$(echo "${list}" | xsetwacom list devices | awk '/stylus/{print $8}')
 
 if [ -z "${pad}" ]; then
     exit 0
@@ -18,5 +18,6 @@ fi
 # configure the buttons on ${stylus} with your xsetwacom commands...
 #xsetwacom set "${stylus}" Button 2 11
 #...
+echo ${stylus}
 xsetwacom  set ${stylus} MapToOutput HEAD-1
 xsetwacom set ${stylus} area 0 0 21600 12150
