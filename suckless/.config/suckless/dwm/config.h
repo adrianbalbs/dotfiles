055dd50799e904d1ead5336b6420b3ac0e3ff93e
adrianbalbs <adrianbalbs@hotmail.com>
2023-07-23 09:53:55 +1000

    reenable systray

--- suckless/.config/suckless/dwm/config.h
+++ suckless/.config/suckless/dwm/config.h
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int horizpadbar        = 15;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 6;        /* vertical padding for statusbar */
-static const int showsystray        = 0;        /* 0 means no systray */
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12:antialias=true", 
