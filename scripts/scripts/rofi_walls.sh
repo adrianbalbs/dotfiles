7ce01a71acf82c6a4dd2c57ea8e3410389c43c86
adrianbalbs <adrianbalbs@hotmail.com>
2023-08-30 15:05:32 +1000

    Config changes

--- scripts/scripts/rofi_walls.sh
+++ scripts/scripts/rofi_walls.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-chosen=$(find ~/Pictures/wallpapers -type f -printf "%f\n"|
-    rofi -dmenu -i -p "Wallpaper:")
+chosen=$(find ~/Pictures/wallpapers -type f -printf "%f\n" |
+	rofi -dmenu -i -p "Wallpaper:")
 
 if [ -n "$chosen" ]; then
-    feh --bg-fill '/home/adrian/Pictures/wallpapers/'"$chosen" 
+	feh --bg-fill --no-xinerama '/home/adrian/Pictures/wallpapers/'"$chosen"
 else
-    exit 1
+	exit 1
 fi
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -6,3 +6,5 @@ fi
 
 # Created by `pipx` on 2023-08-10 09:20:57
 export PATH="$PATH:/home/adrian/.local/bin"
+export PATH="$PATH:/home/adrian/go/bin"
+export PATH=/home/adrian/scripts:$PATH
