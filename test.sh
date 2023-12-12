9909f774462d4723ecd2dd1453ad388fd40db680
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2023-12-12 20:16:06 +1100

    another backup

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ do
     if [ -d "$dir" ] && [ "$dir" != "zsh" ] && [ "$dir" != "x11" ]; then
         if [ -d "$dir/.config/$dir" ]; then
             echo "$dir"
-            cp -vr "$dir/.config/$dir" .
+            cp -vr "$dir/.config/$dir/" . && rm -r "$dir/.config"
         fi
     fi
 done
