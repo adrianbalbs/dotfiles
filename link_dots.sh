d5f0af906a4765cdb20909573eaa0f33875d8495
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-11-26 20:31:36 +1100

    formatting

--- link_dots.sh
+++ link_dots.sh
@@ -1,6 +1,5 @@
 # Stow files stored in $XDG_CONFIG_HOME
-for dir in *
-do
+for dir in *; do
     if [ -d "$dir" ]; then
         if [ ! -d "$HOME/.config/$dir" ]; then
             mkdir "$HOME/.config/$dir"
