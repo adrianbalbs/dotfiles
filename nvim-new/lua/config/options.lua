35c895cb23b1c57aea1c960b12838cc017f76ff5
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-11-27 20:23:08 +1100

    get basic lsp setup for lua

--- nvim-new/lua/config/options.lua
+++ nvim-new/lua/config/options.lua
@@ -11,3 +11,4 @@ vim.opt.scrolloff = 8
 vim.opt.smartindent = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+vim.opt.signcolumn = "yes"
---
+++ nvim-new/lua/plugins/lsp.lua
@@ -0,0 +1,171 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		opts = {},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{
+				"L3MON4D3/LuaSnip",
+				-- follow latest release.
+				version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+				-- install jsregexp (optional!).
+				build = "make install_jsregexp",
+			},
+			dependencies = {
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
+			},
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			luasnip.config.setup({})
+
+			cmp.setup({
+				sources = {
+					{
+						name = "lazydev",
+						-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+						group_index = 0,
+					},
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-f>"] = cmp.mapping(function(fallback)
+						if luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					-- Jump to the previous snippet placeholder
+					["<C-b>"] = cmp.mapping(function(fallback)
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				completion = { completeopt = "menu,menuone,noinsert" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		config = function()
+			local lsp_defaults = require("lspconfig").util.default_config
+
+			-- Add cmp_nvim_lsp capabilities settings to lspconfig
+			-- This should be executed before you configure any language server
+			lsp_defaults.capabilities =
+				vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+			-- LspAttach is where you enable features that only work
+			-- if there is a language server active in the file
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local opts = { buffer = event.buf }
+					vim.keymap.set(
+						"n",
+						"K",
+						vim.lsp.buf.hover,
+						vim.tbl_extend("force", opts, { desc = "Show hover information" })
+					)
+					vim.keymap.set(
+						"n",
+						"gd",
+						vim.lsp.buf.definition,
+						vim.tbl_extend("force", opts, { desc = "Go to definition" })
+					)
+					vim.keymap.set(
+						"n",
+						"gD",
+						vim.lsp.buf.declaration,
+						vim.tbl_extend("force", opts, { desc = "Go to declaration" })
+					)
+					vim.keymap.set(
+						"n",
+						"gi",
+						vim.lsp.buf.implementation,
+						vim.tbl_extend("force", opts, { desc = "Go to implementation" })
+					)
+					vim.keymap.set(
+						"n",
+						"go",
+						vim.lsp.buf.type_definition,
+						vim.tbl_extend("force", opts, { desc = "Go to type definition" })
+					)
+					vim.keymap.set(
+						"n",
+						"gr",
+						vim.lsp.buf.references,
+						vim.tbl_extend("force", opts, { desc = "Find references" })
+					)
+					vim.keymap.set(
+						"n",
+						"gs",
+						vim.lsp.buf.signature_help,
+						vim.tbl_extend("force", opts, { desc = "Show signature help" })
+					)
+					vim.keymap.set(
+						"n",
+						"<F2>",
+						vim.lsp.buf.rename,
+						vim.tbl_extend("force", opts, { desc = "Rename symbol" })
+					)
+					vim.keymap.set({ "n", "x" }, "<F3>", function()
+						vim.lsp.buf.format({ async = true })
+					end, vim.tbl_extend("force", opts, { desc = "Format code" }))
+					vim.keymap.set(
+						"n",
+						"<F4>",
+						vim.lsp.buf.code_action,
+						vim.tbl_extend("force", opts, { desc = "Show code actions" })
+					)
+				end,
+			})
+
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+				handlers = {
+					-- this first function is the "default handler"
+					-- it applies to every language server without a "custom handler"
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+				},
+			})
+		end,
+	},
+}
