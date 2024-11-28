5c53e47bf8306d0bb4f2e7b26db0ef1eb5425132
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-11-28 23:53:10 +1100

    refactor lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,128 +1,130 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		opts = {},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		cmd = { "LspInfo", "LspInstall", "LspStart" },
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-		},
-		config = function()
-			local lsp_defaults = require("lspconfig").util.default_config
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        opts = {},
+    },
+    {
+        "neovim/nvim-lspconfig",
+        cmd = { "LspInfo", "LspInstall", "LspStart" },
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+        },
+        config = function()
+            local lsp_defaults = require("lspconfig").util.default_config
 
-			-- Add cmp_nvim_lsp capabilities settings to lspconfig
-			-- This should be executed before you configure any language server
-			lsp_defaults.capabilities =
-				vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+            -- Add cmp_nvim_lsp capabilities settings to lspconfig
+            -- This should be executed before you configure any language server
+            lsp_defaults.capabilities =
+                vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			-- LspAttach is where you enable features that only work
-			-- if there is a language server active in the file
-			vim.api.nvim_create_autocmd("LspAttach", {
-				desc = "LSP actions",
-				callback = function(event)
-					local opts = { buffer = event.buf }
-					vim.keymap.set(
-						"n",
-						"K",
-						vim.lsp.buf.hover,
-						vim.tbl_extend("force", opts, { desc = "Show hover information" })
-					)
-					vim.keymap.set(
-						"n",
-						"gd",
-						vim.lsp.buf.definition,
-						vim.tbl_extend("force", opts, { desc = "Go to definition" })
-					)
-					vim.keymap.set(
-						"n",
-						"gD",
-						vim.lsp.buf.declaration,
-						vim.tbl_extend("force", opts, { desc = "Go to declaration" })
-					)
-					vim.keymap.set(
-						"n",
-						"gi",
-						vim.lsp.buf.implementation,
-						vim.tbl_extend("force", opts, { desc = "Go to implementation" })
-					)
-					vim.keymap.set(
-						"n",
-						"go",
-						vim.lsp.buf.type_definition,
-						vim.tbl_extend("force", opts, { desc = "Go to type definition" })
-					)
-					vim.keymap.set(
-						"n",
-						"gr",
-						vim.lsp.buf.references,
-						vim.tbl_extend("force", opts, { desc = "Find references" })
-					)
-					vim.keymap.set(
-						"n",
-						"gs",
-						vim.lsp.buf.signature_help,
-						vim.tbl_extend("force", opts, { desc = "Show signature help" })
-					)
-					vim.keymap.set(
-						"n",
-						"<F2>",
-						vim.lsp.buf.rename,
-						vim.tbl_extend("force", opts, { desc = "Rename symbol" })
-					)
-					vim.keymap.set({ "n", "x" }, "<F3>", function()
-						vim.lsp.buf.format({ async = true })
-					end, vim.tbl_extend("force", opts, { desc = "Format code" }))
-					vim.keymap.set(
-						"n",
-						"<F4>",
-						vim.lsp.buf.code_action,
-						vim.tbl_extend("force", opts, { desc = "Show code actions" })
-					)
-				end,
-			})
+            -- LspAttach is where you enable features that only work
+            -- if there is a language server active in the file
+            vim.api.nvim_create_autocmd("LspAttach", {
+                desc = "LSP actions",
+                callback = function(event)
+                    local opts = { buffer = event.buf }
+                    vim.keymap.set(
+                        "n",
+                        "K",
+                        vim.lsp.buf.hover,
+                        vim.tbl_extend("force", opts, { desc = "Show hover information" })
+                    )
+                    vim.keymap.set(
+                        "n",
+                        "gd",
+                        vim.lsp.buf.definition,
+                        vim.tbl_extend("force", opts, { desc = "Go to definition" })
+                    )
+                    vim.keymap.set(
+                        "n",
+                        "gD",
+                        vim.lsp.buf.declaration,
+                        vim.tbl_extend("force", opts, { desc = "Go to declaration" })
+                    )
+                    vim.keymap.set(
+                        "n",
+                        "gi",
+                        vim.lsp.buf.implementation,
+                        vim.tbl_extend("force", opts, { desc = "Go to implementation" })
+                    )
+                    vim.keymap.set(
+                        "n",
+                        "go",
+                        vim.lsp.buf.type_definition,
+                        vim.tbl_extend("force", opts, { desc = "Go to type definition" })
+                    )
+                    vim.keymap.set(
+                        "n",
+                        "gr",
+                        vim.lsp.buf.references,
+                        vim.tbl_extend("force", opts, { desc = "Find references" })
+                    )
+                    vim.keymap.set(
+                        "n",
+                        "gs",
+                        vim.lsp.buf.signature_help,
+                        vim.tbl_extend("force", opts, { desc = "Show signature help" })
+                    )
+                    vim.keymap.set(
+                        "n",
+                        "<F2>",
+                        vim.lsp.buf.rename,
+                        vim.tbl_extend("force", opts, { desc = "Rename symbol" })
+                    )
+                    vim.keymap.set({ "n", "x" }, "<F3>", function()
+                        vim.lsp.buf.format({ async = true })
+                    end, vim.tbl_extend("force", opts, { desc = "Format code" }))
+                    vim.keymap.set(
+                        "n",
+                        "<F4>",
+                        vim.lsp.buf.code_action,
+                        vim.tbl_extend("force", opts, { desc = "Show code actions" })
+                    )
+                end,
+            })
 
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"eslint",
-					"vtsls",
-					"tailwindcss",
-					"html",
-					"cssls",
-				},
-				handlers = {
-					-- this first function is the "default handler"
-					-- it applies to every language server without a "custom handler"
-					function(server_name)
-						require("lspconfig")[server_name].setup({})
-					end,
-					lua_ls = function()
-						require("lspconfig").lua_ls.setup({
-							settings = {
-								Lua = {
-									runtime = {
-										version = "LuaJIT",
-									},
-									diagnostics = {
-										globals = { "vim" },
-									},
-									workspace = {
-										library = {
-											vim.env.VIMRUNTIME,
-										},
-									},
-								},
-							},
-						})
-					end,
-				},
-			})
-		end,
-	},
+            local servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            runtime = {
+                                version = "LuaJIT",
+                            },
+                            diagnostics = {
+                                globals = { "vim" },
+                            },
+                            workspace = {
+                                library = {
+                                    vim.env.VIMRUNTIME,
+                                },
+                            },
+                        },
+                    }
+                }
+            }
+
+            local ensure_installed = vim.tbl_keys(servers or {})
+            require('mason').setup()
+            vim.list_extend(ensure_installed, {
+                "stylua",
+                "prettierd"
+            })
+
+            require("mason-lspconfig").setup({
+                ensure_installed = ensure_installed,
+                handlers = {
+                    function(server_name)
+                        local server = servers[server_name] or {}
+                        server.capabilities = vim.tbl_deep_extend('force', {}, lsp_defaults.capabilities,
+                            server.capabilities or {})
+                        require("lspconfig")[server_name].setup(server)
+                    end,
+                },
+            })
+        end,
+    },
 }
