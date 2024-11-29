986fec4905f05a2aed76e18ba9a93e05f0d9427d
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-11-29 12:13:14 +1100

    modify oil settings

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,12 +2,16 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
     config = function()
-        require("oil").setup()
+        -- Idk why the opts field isn't working
+        require("oil").setup {
+            view_options = {
+                show_hidden = true,
+            },
+        }
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
         vim.keymap.set("n", "<space>-", require("oil").toggle_float, { desc = "Open parent directory float" })
     end,
