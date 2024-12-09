aa372f2ee28330831b9b52e328cbc207e4ac3a41
Adrian <adrian@dymocks.education>
2024-12-09 11:09:14 +1100

    fix vue lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -90,7 +90,35 @@ return {
                         },
                     },
                 },
-                vtsls = {},
+                volar = {
+                    init_options = {
+                        vue = {
+                            hybridMode = true,
+                        },
+                    },
+                },
+                vtsls = {
+                    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+                    settings = {
+                        vtsls = {
+                            -- autoUseWorkspaceTsdk = true,
+                            tsserver = {
+                                globalPlugins = {
+                                    {
+                                        name = "@vue/typescript-plugin",
+                                        location = require("mason-registry")
+                                            .get_package("vue-language-server")
+                                            :get_install_path()
+                                            .. "/node_modules/@vue/language-server",
+                                        languages = { "vue" },
+                                        configNamespace = "typescript",
+                                        enableForWorkspaceTypeScriptVersions = true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
                 rust_analyzer = { enabled = false },
                 gopls = {},
                 basedpyright = {
