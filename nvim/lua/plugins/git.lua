deb14eec1377a76bd11f046c787bbcd2f960e461
Adrian <adrian@dymocks.education>
2025-01-30 11:02:10 +1100

    make neogit open in a split

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -8,7 +8,9 @@ return {
         },
         config = function()
             local neogit = require "neogit"
-            vim.keymap.set("n", "<leader>gg", neogit.open, { desc = "Open neogit" })
+            vim.keymap.set("n", "<leader>gg", function()
+                neogit.open { kind = "auto" }
+            end, { desc = "Open neogit" })
         end,
     },
     {
