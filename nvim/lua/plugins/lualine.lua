4d88b620e8ba02c325494432fc68f665f6d563b1
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2025-02-19 10:12:59 +1100

    macros status not showing

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,21 @@ return {
     -- enabled = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
+        sections = {
+            lualine_x = {
+                {
+                    require("noice").api.statusline.mode.get,
+                    cond = require("noice").api.statusline.mode.has,
+                    color = { fg = "#ff9e64" },
+                },
+
+                "encoding",
+                "fileformat",
+                "filetype",
+            },
+        },
         options = {
+
             section_separators = "",
             component_separators = "",
             globalstatus = true,
