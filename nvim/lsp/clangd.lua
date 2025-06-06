8237fe08173095995f835c4450c082d3712ca579
adrianbalbs <adrianbalbs@hotmail.com>
2025-06-06 10:35:13 +1000

    clangd settings

--- nvim/lsp/clangd.lua
+++ nvim/lsp/clangd.lua
@@ -1,5 +1,12 @@
 return {
-    cmd = { "clangd", "--background-index" },
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--completion-style=detailed",
+        "--header-insertion=iwyu",
+        "--function-arg-placeholders",
+    },
     root_markers = { "compile_commands.json", "compile_flags.txt" },
     filetypes = { "c", "cpp" },
 }
